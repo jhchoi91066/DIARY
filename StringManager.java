19f8b9e41b9ddef3c0b87cb9b72731fba444ff40
최진호 <127467867+jhchoi91066@users.noreply.github.com>
2023-12-01 20:00:08 +0900

    Create StringManager.java

---
+++ StringManager.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class StringManager extends Component {
+
+    //넘겨받은 인수가 1자리 수 인 경우 앞에 '0' 문자를 붙이고
+    //2자리 수면 그냥 쓴다.
+    public static String getZeroString(int n) {
+        return (n < 10) ? "0" + n : Integer.toString(n);
+    }
+}
+
+
+
+
+
