694f484a6382df11712d95e8b8bda691017d6d71
jsh02 <127283576+jsh02@users.noreply.github.com>
2023-12-03 23:23:14 +0900

    login.java

--- login.java
+++ login.java
@@ -2,30 +2,30 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
+
 public class login extends JFrame {
     private JTextField passwordField;
     private String storedPassword;
-
     private boolean isPasswordSet = false;
 
     public login() {
-        setTitle("비밀번호 보호");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(600, 600);
-        setLocationRelativeTo(null);
+        setTitle("Diary");  // jframe 의 제목을 설정
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // 닫기 버튼 누르면 코드 종료
+        setSize(600, 600); // jframe 의 크기를 설정합니다.
+        setLocationRelativeTo(null); // jframe을 중앙에 배치합니다
 
         JPanel mainPanel = new JPanel(); // Renamed the panel variable to mainPanel
         mainPanel.setLayout(new BorderLayout());
 
         JLabel titleLabel = new JLabel("My Diary", SwingConstants.CENTER);
-        titleLabel.setFont(new Font("Arial", Font.BOLD, 40)); // Setting the font size to 40
-        mainPanel.add(titleLabel, BorderLayout.NORTH);
+        titleLabel.setFont(new Font("Arial", Font.BOLD, 100)); // 글자의 크기 폰트 굵기를 정합니다.
+        mainPanel.add(titleLabel, BorderLayout.NORTH); // 메인패널을 추가하는 코드
 
 
         JPanel panel = new JPanel();
         panel.setLayout(new GridLayout(3, 1));
 
-        JLabel passwordLabel = new JLabel("비밀번호 입력:");
+        JLabel passwordLabel = new JLabel("비밀번호 입력",SwingConstants.CENTER);
         panel.add(passwordLabel);
 
         passwordField = new JPasswordField();
@@ -67,7 +67,7 @@ public class login extends JFrame {
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
                 // 로그인 성공 시 다이어리 클래스 열기
-                new diary();
+                new Diary();
             }
         });
         dispose(); // 로그인 창 닫기
